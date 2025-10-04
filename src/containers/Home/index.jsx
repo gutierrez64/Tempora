6608f32c9b55f0ad47e2431351e262a6cf4df6b2
gutierrez64 <gutierrezmatheus210@gmail.com>
2025-10-04 12:57:35 -0300

    Home page styles

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 function Home() {
     const navigate = useNavigate();
     return(
-        <div className="container">
+        <div className="home">
             <img src="../../../public/icon-transparent.png" alt="icon" />
             <button onClick={() => navigate("/map")}>Start</button>
         </div>

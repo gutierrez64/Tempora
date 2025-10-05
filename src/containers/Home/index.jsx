650bacf50c1c25525257eaa98e203733497ac1ba
gutierrez64 <gutierrezmatheus210@gmail.com>
2025-10-05 18:16:40 -0300

    vercel version test

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -5,7 +5,7 @@ function Home() {
     const navigate = useNavigate();
     return (
         <div className="home">
-            <img src="../../../public/icon-transparent.png" alt="icon" />
+            <img src="/icon-transparent.png" alt="icon" />
             <button onClick={() => navigate("/map")}>Start</button>
             <div className="attribution">
                 <p>

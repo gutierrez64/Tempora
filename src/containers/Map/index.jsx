50bbe579eafb8fb6c92caf514d560200b83ff87e
gutierrez64 <gutierrezmatheus210@gmail.com>
2025-10-05 18:27:53 -0300

    test fixing markers image bug

--- src/containers/Map/index.jsx
+++ src/containers/Map/index.jsx
@@ -4,7 +4,17 @@ import { EditControl } from "react-leaflet-draw";
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-draw/dist/leaflet.draw.css';
 import "./styles.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 import L, { geoJSON } from "leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function Map() {
     const [isRotated, setIsRotated] = useState(true);
